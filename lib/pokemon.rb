853c45729216445327bd1eb82b19f7dc31ff4b0f
tiefengeist <sahirnambiar@gmail.com>
2019-02-06 18:08:08 -0800

    Did bonus

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,12 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :db, :hp
 
-   def initialize(id:, name:, type:, db:)
+   def initialize(id:, name:, type:, db:, hp: nil)
     @id = id
     @name = name
     @type = type
     @db = db
+    @hp = hp
   end
 
    def self.save(name, type, db_cxn)
@@ -14,8 +15,12 @@ class Pokemon
 
    def self.find(id, db_cxn)
      i_choose_you = db_cxn.execute("SELECT * FROM pokemon WHERE id = (?)", id)
-     monster = Pokemon.new(id: i_choose_you[0][0], name: i_choose_you[0][1], type: i_choose_you[0][2], db: db_cxn)
+     monster = Pokemon.new(id: i_choose_you[0][0], name: i_choose_you[0][1], type: i_choose_you[0][2], hp: i_choose_you[0][3], db: db_cxn)
      return monster
+   end
+
+   def alter_hp(new_hp, db_cxn)
+     damage = db_cxn.execute("UPDATE pokemon SET hp = #{new_hp} WHERE id = (?)", id)
    end
 
 
